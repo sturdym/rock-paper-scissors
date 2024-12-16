fb66085aaaca52adec0589c5cc4925375002114c
Mark Sturdy <uitmst@leeds.ac.uk>
2024-12-16 09:45:40 +0000

    initial commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace rock_paper_scissors
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
